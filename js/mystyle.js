7803d002f158817eb56404b9f0582be912c07d25
Patryk Kollar <patryk.kollar@outlook.com>
2017-05-25 14:30:31 +0200

    bug fix v1

--- js/mystyle.js
+++ js/mystyle.js
@@ -1,15 +1,14 @@
 /**
  * Created by David on 31.03.2017.
  */
-(function($) {
-    "use strict"; // Start of use strict
+$(document).ready(function() {
 
     // jQuery for page scrolling feature - requires jQuery Easing plugin
     $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
+        var anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
-        }, 12, 'easeInOutExpo');
+            scrollTop: ($(anchor.attr('href')).offset().top - 50)
+        }, 500, 'linear');
         event.preventDefault();
     });
 
@@ -31,7 +30,7 @@
         offset: {
             top: 100
         }
-    })
+    });
 
 
     // Get the modal
@@ -47,12 +46,12 @@
 // When the user clicks the button, open the modal
     btn.onclick = function() {
         modal.style.display = "block";
-    }
+    };
 
 // When the user clicks on <span> (x), close the modal
     span.onclick = function() {
         modal.style.display = "none";
-    }
+    };
 
 // When the user clicks anywhere outside of the modal, close it
     window.onclick = function(event) {
@@ -74,20 +73,20 @@ var span = document.getElementsByClassName("close1")[0];
 // When the user clicks the button, open the modal
 btn.onclick = function() {
     modal1.style.display = "block";
-}
+};
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal1.style.display = "none";
-}
+};
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal1) {
         modal1.style.display = "none";
     }
-}
+};
 
 ///https://stackoverflow.com/questions/34427926/using-multiple-modal-dialogs-in-one-page
 
-})(jQuery); // End of use strict
+});
